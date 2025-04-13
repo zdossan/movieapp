456f486c307568de0c50559d9dcf4d4620103c61
Zhanerke Dossan <zhanerke.dossan@nu.edu.kz>
2025-04-13 23:34:42 +0500

    add movie search API integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
 import "./css/App.css";
 import Favorites from "./pages/Favorites";
 import Home from "./pages/Home";
-import {Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import { MovieProvider } from "./contexts/MovieContext";
 import NavBar from "./components/NavBar";
 
 function App() {
-  
   return (
-    <div>
+    <MovieProvider>
       <NavBar />
-    <main className="main-content">
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/favorites" element={<Favorites />}/>
-
-      </Routes>
-    </main>
-    </div>
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </MovieProvider>
   );
 }
 
