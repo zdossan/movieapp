837446188d6f9c78c2f1139704a08640f1a4ca1f
Zhanerke Dossan <zhanerke.dossan@nu.edu.kz>
2025-04-13 23:45:09 +0500

    Remove unused component

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import {BrowserRouter} from "react-router-dom"
-import './css/index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./css/index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-      <BrowserRouter>  <App /></BrowserRouter>
-  
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
